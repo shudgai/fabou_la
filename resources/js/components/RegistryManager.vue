be7ce36ab02c57db100d5f77afee8074ec90b601
shudgai <shudgai999@gmail.com>
2026-05-15 09:43:44 +0800

    ui: Registry - shift master and count down while keeping titles stationary

--- resources/js/components/RegistryManager.vue
+++ resources/js/components/RegistryManager.vue
@@ -99,8 +99,8 @@
                                  <div class="font-black tracking-tighter leading-none text-center font-biaokai -mt-3" style="font-size: 12px !important; color: rgb(139, 0, 0) !important; font-family: 'DFKai-SB', '標楷體', 'BiauKai', 'Kaiti TC', serif !important;">法寶登記專區</div>
                                 <div class="font-black text-[#991b1b] tracking-tighter leading-none text-center mt-[2px] font-biaokai" style="font-size: 10.5px !important; font-family: 'DFKai-SB', '標楷體', 'BiauKai', 'Kaiti TC', serif !important;">特殊法寶登記簿</div>
 
-                                <div class="flex-1 flex flex-col items-center justify-center w-full mt-0">
-                                    <div class="font-black tracking-tight leading-tight text-center whitespace-nowrap !font-black font-biaokai -mt-4"
+                                <div class="flex-1 flex flex-col items-center justify-center w-full mt-2">
+                                    <div class="font-black tracking-tight leading-tight text-center whitespace-nowrap !font-black font-biaokai"
                                          :style="'font-size: 18px !important; font-weight: 900 !important; color: ' + (folder.name === '閻王仙師' ? '#000000' : '#dc2626') + ' !important; font-family: \'DFKai-SB\', \'標楷體\', \'BiauKai\', \'Kaiti TC\', serif !important; -webkit-text-stroke: 0.5px ' + (folder.name === '閻王仙師' ? '#000000' : '#dc2626') + ' !important;'">
                                          {{ folder.name === '父皇仙師' ? '父皇' : folder.name }}
                                     </div>
